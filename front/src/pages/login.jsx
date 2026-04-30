110b7e5d3f31ddc32301423858ec485cb8aef70c
Joao Paulo Muniz <munizjoaopaulo21@gmail.com>
2026-04-30 16:11:08 -0300

    3001 no login

--- front/src/pages/login.jsx
+++ front/src/pages/login.jsx
@@ -16,7 +16,7 @@ export default function Login() {
     }
 
     try {
-      const res = await fetch("http://localhost:3000/login", {
+      const res = await fetch("http://localhost:3001/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
